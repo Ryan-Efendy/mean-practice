bfbdb6bee304fe296f6fb1d5269a94e8d4e6e265
Ryan Efendy <ryanefendy95@gmail.com>
2018-12-05 08:41:28 -0800

    CHORE: update eslint + prettier to work togetherd

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,14 @@ const app = express();
 
 app.use('/api/posts', (req, res, next) => {
   const posts = [
-    { id: '1234567890', title: 'first post', content: "this is the first's post content" },
-    { id: '1234567890', title: 'first post', content: "this is the first's post content" },
-    { id: '1234567890', title: 'first post', content: "this is the first's post content" }
+    { id: '1234567890', title: 'first post', content: `this is the first's post content` },
+    { id: '1234567890', title: 'first post', content: `this is the first's post content` },
+    { id: '1234567890', title: 'first post', content: `this is the first's post content` },
   ];
 
   res.status(200).json({
     message: 'Posts fetched successfully',
-    posts: posts
+    posts: posts,
   });
 });
 
