0d6a8a22b8fddbd5c9d37fee2b6bf37e530ac120
Ryan Efendy <ryanefendy95@gmail.com>
2018-12-05 00:53:25 -0800

    FEAT: add node & express

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    { id: '1234567890', title: 'first post', content: "this is the first's post content" },
+    { id: '1234567890', title: 'first post', content: "this is the first's post content" },
+    { id: '1234567890', title: 'first post', content: "this is the first's post content" }
+  ];
+
+  res.status(200).json({
+    message: 'Posts fetched successfully',
+    posts: posts
+  });
+});
+
+module.exports = app;
