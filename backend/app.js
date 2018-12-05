be07a89d1a55b3e15967cb0ca038d056d62ad3fd
Ryan Efendy <ryanefendy95@gmail.com>
2018-12-05 10:04:47 -0800

    FEAT: add connection to mongodb atlas

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
+
+mongoose
+  .connect('mongodb+srv://ryan:fLUMt7l9Zjpvxomg@cluster0-hyran.mongodb.net/test?retryWrites=true')
+  .then(() => {
+    console.log('connected to database!');
+  })
+  .catch(() => {
+    console.log('connection failed');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
   next();
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
   console.log(post);
   res.status(201).json({
-    message: 'Post',
+    message: 'Post ',
   });
 });
 
