167a676e5c7d7635f6bee23604abd2c5237651ab
Ryan Efendy <ryanefendy95@gmail.com>
2018-12-06 22:06:23 -0800

    REFACTOR: reorganize backend routes

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
-
-const Post = require('./models/post');
+const postsRouter = require('./routes/posts');
 
 mongoose
   .connect('mongodb+srv://ryan:fLUMt7l9Zjpvxomg@cluster0-hyran.mongodb.net/node-angular?retryWrites=true')
@@ -24,51 +23,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'Post added successfully!',
-      postId: createdPost._id,
-    });
-  });
-});
-
-app.get('/api/posts', (req, res, next) => {
-  Post.find().then(documents => {
-    res.status(200).json({
-      message: 'Posts fetched successfully',
-      posts: documents,
-    });
-  });
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    post ? res.status(200).json(post) : res.status(400).json({ message: 'Post not found' });
-  });
-});
-
-app.delete('/api/posts/:id', (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    // console.log(result);
-    res.status(200).json({ message: 'post deleted!' });
-  });
-});
-
-app.put('/api/posts/:id', (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-  });
-  Post.updateOne({ _id: req.params.id }, post).then(result => {
-    console.log(result);
-    res.status(200).json({ message: 'Update successful' });
-  });
-});
+app.use('/api/posts', postsRouter);
 
 module.exports = app;
