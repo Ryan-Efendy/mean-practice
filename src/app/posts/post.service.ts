effc69c5f698e93001f4543861adb837f445c0b8
Ryan Efendy <ryanefendy95@gmail.com>
2018-12-12 21:51:27 -0800

    FEAT: add global angular config

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -4,6 +4,9 @@ import { Subject } from 'rxjs'; // like EventEmitter
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { environment } from "../../environments/environment";
+
+const BACKEND_URL = `${environment.apiUrl}/posts/`;
 
 @Injectable({
   providedIn: 'root',
@@ -18,7 +21,7 @@ export class PostService {
   getPosts(postsPerPage: number, currentPage: number) {
     const queryParams = `?pageSize=${postsPerPage}&page=${currentPage}`;
     this.http
-      .get<{ message: string; posts: any; maxPosts: number }>(`http://localhost:3000/api/posts${queryParams}`)
+      .get<{ message: string; posts: any; maxPosts: number }>(`${BACKEND_URL}${queryParams}`)
       .pipe(
         map(postData => {
           return {
@@ -54,18 +57,18 @@ export class PostService {
     postData.append('title', title);
     postData.append('content', content);
     postData.append('image', image, title);
-    this.http.post<{ message: string; post: Post }>('http://localhost:3000/api/posts', postData).subscribe(() => {
+    this.http.post<{ message: string; post: Post }>(`${BACKEND_URL}posts`, postData).subscribe(() => {
       this.router.navigate(['/']);
     });
   }
 
   deletePost(postId: string) {
-    return this.http.delete(`http://localhost:3000/api/posts/${postId}`);
+    return this.http.delete(`${BACKEND_URL}${postId}`);
   }
 
   getPost(postId: string) {
     return this.http.get<{ _id: string; title: string; content: string; imagePath: string, creator: string }>(
-      `http://localhost:3000/api/posts/${postId}`
+      `${BACKEND_URL}${postId}`
     );
   }
 
@@ -86,7 +89,7 @@ export class PostService {
         creator: null
       };
     }
-    this.http.put('http://localhost:3000/api/posts/' + id, postData).subscribe(() => {
+    this.http.put(`${BACKEND_URL}id`, postData).subscribe(() => {
       this.router.navigate(['/']);
     });
   }
